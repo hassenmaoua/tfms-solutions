510c1f50e8520aa8a152f57b430523d1134567f2
Hassen Maoua <hassenmaoua07@gmail.com>
2022-10-13 18:46:05 +0100

    Some CSS update

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,14 +4,9 @@ import { Link } from 'react-router-dom';
 function NotFound() {
   const container = {
     height: 300,
+    width: '100%',
     position: 'absolute',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
     textAlign: 'center',
-
-    left: 'calc(50% - 515px / 2)',
     top: 'calc(50% - 300px / 2)',
   };
 
@@ -25,12 +20,12 @@ function NotFound() {
     <div style={container}>
       <h1 style={h404}>404</h1>
       <h1>Ooups, page not found</h1>
-      <h5>
+      <h4>
         Nous sommes vraiment désolés pour ce désagrément. Il semble que vous
         essayez d'accéder à <br /> une page qui a été supprimée ou qui n'a
         jamais existé.
-      </h5>
-      <Link to='/'>Retour à la page d'accueil</Link>
+      </h4>
+      <Link to='/'>Retourner à la page d'accueil</Link>
     </div>
   );
 }
